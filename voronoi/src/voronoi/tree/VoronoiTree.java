516439d10b5ef9e83ae9bb56dd3b9cd1a4229796
Sergio Santamaria <s.santamaria.most@gmail.com>
2016-05-21 20:02:51 +0200

    Añadido método rebalance y asociados que se usen

--- voronoi/src/voronoi/tree/VoronoiTree.java
+++ voronoi/src/voronoi/tree/VoronoiTree.java
@@ -44,13 +44,149 @@ public class VoronoiTree extends LinkedBinaryTree<Pareja> {
             this.insertRight(pos, new Pareja(nueva.getDerecho()));
             referencia = pos;
         }
-        this.rebalance();
+        this.rebalance(this.root());
         return referencia;
     }
     
+    private int calculateHeight(Position<Pareja> p) {
+        if(this.isLeaf(p)){
+            return 1;
+        }else{
+            int alturaIzquierda = 1 + calculateHeight(this.left(p));
+            int alturaDerecha = 1 + calculateHeight(this.right(p));
+            return Math.max(alturaDerecha, alturaIzquierda);
+        }
+    }
     
-    private void rebalance() {
-        throw new UnsupportedOperationException("TO DO: rebalance()");
+    
+    private void rebalance(Position<Pareja> zPos) {
+        if (this.isInternal(zPos)) {
+            calculateHeight(zPos);
+        }
+        while (!this.isRoot(zPos)) { // traverse up the tree towards the
+            // root
+            zPos = this.parent(zPos);
+            calculateHeight(zPos);
+            if (!isBalanced(zPos)) {
+                // perform a trinode restructuring at zPos's tallest grandchild
+                Position<Pareja> xPos = tallerChild(tallerChild(zPos));
+                zPos = restructure(xPos);
+                calculateHeight(this.left(zPos));
+                calculateHeight(this.right(zPos));
+                calculateHeight(zPos);
+            }
+        }
+    }
+    
+    private boolean isBalanced(Position<Pareja> p) {
+        int leftHeight = (this.hasLeft(p))?calculateHeight(this.left(p)):0;
+        int rightHeight = (this.hasRight(p))?calculateHeight(this.right(p)):0;
+        final int bf = leftHeight - rightHeight;
+        return ((-1 <= bf) && (bf <= 1));
+    }
+    
+    public Position<Pareja> restructure(Position<Pareja> posNode) {
+        BTNode<Pareja> lowKey, midKey, highKey, t1, t2, t3, t4;
+        Position<Pareja> posParent = this.parent(posNode); // assumes x has a parent
+        Position<Pareja> posGrandParent = this.parent(posParent); // assumes y has a parent
+//        boolean nodeLeft = (posNode == bst.binTree.left(posParent));
+        boolean nodeLeft = (this.hasLeft(posParent) && (posNode == this.left(posParent)));
+//        boolean parentLeft = (posParent == bst.binTree.left(posGrandParent));
+        boolean parentLeft = (this.hasLeft(posGrandParent) && (posParent == this.left(posGrandParent)));
+        BTNode<Pareja> node = (BTNode<Pareja>) posNode, parent = (BTNode<Pareja>) posParent, grandParent = (BTNode<Pareja>) posGrandParent;
+        if (nodeLeft && parentLeft) {// Desequilibrio izda-izda
+            lowKey = node;
+            midKey = parent;
+            highKey = grandParent;
+            t1 = lowKey.getLeft();
+            t2 = lowKey.getRight();
+            t3 = midKey.getRight();
+            t4 = highKey.getRight();
+        } else if (!nodeLeft && parentLeft) {// Desequilibrio izda-dcha
+            lowKey = parent;
+            midKey = node;
+            highKey = grandParent;
+            t1 = lowKey.getLeft();
+            t2 = midKey.getLeft();
+            t3 = midKey.getRight();
+            t4 = highKey.getRight();
+        } else if (nodeLeft && !parentLeft) {// Desequilibrio dcha-izda
+            lowKey = grandParent;
+            midKey = node;
+            highKey = parent;
+            t1 = lowKey.getLeft();
+            t2 = midKey.getLeft();
+            t3 = midKey.getRight();
+            t4 = highKey.getRight();
+        } else { // Desequilibrio dcha-dcha
+            lowKey = grandParent;
+            midKey = parent;
+            highKey = node;
+            t1 = lowKey.getLeft();
+            t2 = midKey.getLeft();
+            t3 = highKey.getLeft();
+            t4 = highKey.getRight();
+        }
+
+        // put b at z's place
+        if (this.isRoot(posGrandParent)) {
+            this.subTree(midKey);
+        } else {
+            BTNode<Pareja> zParent = (BTNode<Pareja>) this.parent(posGrandParent);
+            if (posGrandParent == this.left(zParent)) {
+                midKey.setParent(zParent);
+                zParent.setLeft(midKey);
+            } else { // z was a right child
+                midKey.setParent(zParent);
+                zParent.setRight(midKey);
+            }
+        }
+        // place the rest of the nodes and their children
+        midKey.setLeft(lowKey);
+        lowKey.setParent(midKey);
+        midKey.setRight(highKey);
+        highKey.setParent(midKey);
+        lowKey.setLeft(t1);
+        if (t1 != null) {
+            t1.setParent(lowKey);
+        }
+        lowKey.setRight(t2);
+        if (t2 != null) {
+            t2.setParent(lowKey);
+        }
+        highKey.setLeft(t3);
+        if (t3 != null) {
+            t3.setParent(highKey);
+        }
+        highKey.setRight(t4);
+        if (t4 != null) {
+            t4.setParent(highKey);
+        }
+        this.subTree(this.root());
+        return midKey; // the new root of this subtree
+    }
+    
+    private Position<Pareja> tallerChild(Position<Pareja> p) {
+
+        int leftHeight = (this.hasLeft(p))?calculateHeight(this.left(p)):0;
+        int rightHeight = (this.hasRight(p))?calculateHeight(this.right(p)):0;
+
+        if (leftHeight > rightHeight) {
+            return this.left(p);
+        } else if (leftHeight < rightHeight) {
+            return this.right(p);
+        }
+
+        // equal height children - break tie using parent's type
+        if (this.isRoot(p)) {
+            return this.left(p);
+        }
+
+        if (p == this.left(this.parent(p))) {
+            return this.left(p);
+        } else {
+            return this.right(p);
+        }
     }
     
     private boolean deterFrenteParabolas(Punto a, Punto b, Punto site) {    //a izquierdo, b derecho
